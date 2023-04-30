e7139fc51fc6c2adfc967d2f06aede7ffc289344
DarthJAP <pintoaj03@gmail.com>
2023-04-29 22:30:01 -0400

    Made resident page

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from flask import *
 
 app = Flask(__name__)
 
+guests = [ {"name": "Bobby Fischer", "license": "123456789", "duration": "24", "phone": "416-614-4114", "email": "ruh-roh@goofball.com"}, {"name": "Ubuntu", "email": "duh-doh@boofgall.com"} ]
 
 @app.route("/")
 def hello():
-    return render_template("home.html", arg1 = "Hello", arg2 = "World"
+    return render_template("resident.html", name = "Magnus 'Opus' Carlson", address = "3579 True Road", unit = 101, hasGuest = True, guests = guests)
 
 
 @app.route("/test")
